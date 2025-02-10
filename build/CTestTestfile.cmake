86af9559926cf03f79104c8373bc29fbef8d2167
Arnab Dev <official.arnabdev@gmail.com>
2025-02-10 12:45:38 -0600

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/arnab/poppler-21.07.0
+# Build directory: /home/arnab/poppler-21.07.0/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("utils")
+subdirs("test")
+subdirs("cpp")
---
+++ build/utils/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/arnab/poppler-21.07.0/utils
+# Build directory: /home/arnab/poppler-21.07.0/build/utils
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
